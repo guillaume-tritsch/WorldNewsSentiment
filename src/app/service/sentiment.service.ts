c12e24d436d51753a476dd1d8120e4dbfea0601c
Guillaume Tritsch <guillaume.tritsch@2moro.fr>
2023-08-03 17:30:47 +0200

    e

--- src/app/service/sentiment.service.ts
+++ src/app/service/sentiment.service.ts
@@ -10,17 +10,31 @@ export class SentimentService {
 
   constructor(private _api: APIService) {
     var Sentiment = require('sentiment');
-    var sentimental = require('sentimental');
+    var sentimentAnalysis  = require('sentiment-analysis');
+    var winkSentiment  = require('wink-sentiment');
 
     var sentiment = new Sentiment();
     this._api.getPosts().subscribe(e => {
       this.newsList = e.news
       var t: any[] = []
       this.newsList.forEach(e => {
-        console.log(e.title, sentiment.analyze(e.title), sentimental('EN',e.title))
-        t.push(sentiment.analyze(e.title))
+        console.log(e.title, sentiment.analyze(e.title).score, sentimentAnalysis(e.title), winkSentiment(e.title).score)
+        t.push(sentiment.analyze(e.title).score)
       })
+
+      function isPositive(value: number) {
+        return value != 0;
+    }
+
+      t = t.filter(isPositive)
       console.log(t)
+
+      var sum = 0;
+      t.forEach(e => {
+        sum += e;
+      })
+      console.log(sum / t.length, t[Math.floor(t.length/2)], (t[Math.floor(t.length/2)] + sum / t.length) / 2)
+
     });
   }
 }
